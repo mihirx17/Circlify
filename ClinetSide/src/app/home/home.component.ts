05c666a05ceef4e29782007fa2caab41dc22a5bf
mihirx17 <mihirkumargupta2@gmail.com>
2025-02-09 14:13:31 +0530

    comments added

--- ClinetSide/src/app/home/home.component.ts
+++ ClinetSide/src/app/home/home.component.ts
@@ -11,26 +11,29 @@ import { ChatService } from '../services/chat.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  messages: string[] = [];
-  messageText: string = '';
-  isChatOpen: boolean = false;
+  messages: string[] = []; // Array to store chat messages
+  messageText: string = ''; // Variable to bind to the input field
+  isChatOpen: boolean = false; // Boolean to track chat window state
 
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService) {} // Inject ChatService
 
   ngOnInit() {
+    // Subscribe to receive messages from the chat service
     this.chatService.receiveMessage().subscribe((message: string) => {
-      this.messages.push(message);
+      this.messages.push(message); // Add received message to messages array
     });
   }
 
   toggleChat() {
+    // Toggle the chat window open/close state
     this.isChatOpen = !this.isChatOpen;
   }
 
   sendMessage() {
+    // Send the message if the input is not empty
     if (this.messageText.trim()) {
-      this.chatService.sendMessage(this.messageText);
-      this.messageText = '';
+      this.chatService.sendMessage(this.messageText); // Send message via ChatService
+      this.messageText = ''; // Clear the input field
     }
   }
 }
